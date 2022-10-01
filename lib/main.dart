0227810f850a045bbb27db416d92df2f106ec5fb
rohan rohokale <rohanrohokale0@gmail.com>
2022-10-01 07:56:50 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -29,60 +29,23 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppStyle.primaryColor,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Center(
-            child: QrImage(
-              data: data,
-              backgroundColor: Colors.white,
-              version: QrVersions.auto,
-              size: 300.0,
-            ),
-          ),
-          SizedBox(
-            height: 24,
-          ),
-          Container(
-            width: 300.0,
-            child: TextField(
-              //we will generate a new qr code when the input value change
-              onChanged: (value) {
-                setState(() {
-                  data = value;
-                });
-              },
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white,
+        backgroundColor: AppStyle.primaryColor,
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Center(
+                child: QrImage(
+                  data:
+                      'https://play.google.com/store/apps/details?id=com.a1.pos',
+                  backgroundColor: Colors.white,
+                  version: QrVersions.auto,
+                  size: 300.0,
+                ),
               ),
-              decoration: InputDecoration(
-                hintText: "Type the Data",
-                filled: true,
-                fillColor: AppStyle.textInputColor,
-                border: InputBorder.none,
+              SizedBox(
+                height: 24,
               ),
-            ),
-          ),
-          SizedBox(
-            height: 24.0,
-          ),
-          RawMaterialButton(
-            onPressed: () {},
-            fillColor: AppStyle.accentColor,
-            shape: StadiumBorder(),
-            padding: EdgeInsets.symmetric(
-              horizontal: 36.0,
-              vertical: 16.0,
-            ),
-            child: Text(
-              "Generate QR Code",
-            ),
-          )
-        ],
-      ),
-    );
+            ]));
   }
 }
